7e52869ac4d830aa08125c6324900228cda331d4
safwan mansuri <safwanmansuri.ict17@gmail.com>
2020-05-27 18:50:43 +0530

    first-commit

---
+++ masks.py
@@ -0,0 +1,84 @@
+# import cv2
+# import numpy as np
+# from tensorflow import keras
+
+# model = keras.models.load_model('maskss.h5')
+# face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+
+# def detect(gray) :
+#     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+#     for (x, y, w, h) in faces :
+#         resized = gray[y:y+w,x:x+w]
+#         resized=cv2.resize(resized,(100,100))
+#         resized = np.expand_dims(resized,axis=0)
+#         # resized.reshape((1,100,100,3))
+#         normalized=resized/255.0
+#         result=model.predict(resized) 
+#         print(result)
+#     return frame  
+
+
+
+# video_capture = cv2.VideoCapture(0)
+# while True :
+#     _, gray = video_capture.read()
+#     # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#     canvas = detect(gray)
+#     cv2.imshow("Video", canvas)
+#     if cv2.waitKey(1) & 0xFF == ord('q') :
+#         break
+
+# video_capture.release()
+# cv2.destroyAllWindows()
+
+
+
+
+from keras.models import load_model
+import cv2
+import numpy as np
+
+model = load_model('maskss.h5')
+face_clsfr=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+source=cv2.VideoCapture(0)
+
+labels_dict={0:'with_mask',1:'without_mask'}
+color_dict={0:(0,255,0),1:(0,0,255)}
+
+while(True):
+
+    ret,gray=source.read()
+    # gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces=face_clsfr.detectMultiScale(gray,1.3,5)  
+
+    for x,y,w,h in faces:
+
+        resized = gray[y:y+w,x:x+w]
+        resized=cv2.resize(resized,(100,100))
+        resized = np.expand_dims(resized,axis=0)
+        normalized=resized/255.0
+        result = model.predict(normalized) 
+        if result[0][0] < 0.5 :
+            ans = 0
+        else :
+            ans = 1
+      
+        cv2.rectangle(gray,(x,y),(x+w,y+h),color_dict[ans],2)
+        cv2.rectangle(gray,(x,y-30),(x+w,y),color_dict[ans],-1)
+        cv2.putText(
+          gray, labels_dict[ans], 
+          (x, y-10),
+          cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2)
+        
+        
+    cv2.imshow('LIVE',gray)
+    key=cv2.waitKey(1)
+    
+    if(key==27):
+        break
+        
+cv2.destroyAllWindows()
+source.release()
